d0cee3b6138e6b4652f166e997249f243191237b
devashen07 <49387772+devashen07@users.noreply.github.com>
2022-02-18 10:55:40 +0200

    forgot to add helper script

---
+++ json-rest-api/helper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MaxResults {
+
+    public Result result;
+
+    [System.Serializable]
+    public class Result
+    {
+        public string src_lang;
+        public List<string> translations;
+        public string tgt_lang;
+        public string utt;
+    }
+
+    public static MaxResults CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<MaxResults>(jsonString);
+    }
+
+}
